9cb394e448c44375b082f3fd9edbb647119cf090
Lucas Andrews <andrews852@gmail.com>
2025-01-27 12:47:25 -0300

    remove console log from hotkeys configuration

--- src/renderer/src/hooks/useHotkeysConfig.ts
+++ src/renderer/src/hooks/useHotkeysConfig.ts
@@ -20,8 +20,6 @@ export default function useHotKeysConfig(): {
       if (res?.length) {
         setHotkeys(res)
       }
-
-      console.log('Hotkeys:', res)
     } catch (err) {
       console.error('Error al cargar hotkeys:', err)
     }
